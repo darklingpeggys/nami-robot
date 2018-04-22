f9b853b0122ad830655048d1a0c1225f7b17106d
碎碎酱 <yinxin630@gmail.com>
2018-04-22 10:27:30 +0800

    feat: 添加注释, 测试消息发送以及消息解析

--- socket/fiora.js
+++ socket/fiora.js
@@ -1,5 +1,10 @@
 const IO = require('socket.io-client');
 
+const groups = {
+    nami: '5adb43c154063970de326fcd'
+    fiora: '5adacdcfa109ce59da3e83d3'
+}
+
 class Fiora {
     constructor() {
         this.name = 'fiora';
@@ -17,9 +22,17 @@ class Fiora {
         });
 
         this.log('开始监听事件');
-        this.socket.on('connect', socket => {
+        this.socket.on('connect', async socket => {
             this.log('connect');
-            this.login('robot10', '421we2fgv34897');
+            // 连接后就可以开始登陆, 将在该连接中一直保持登陆态
+            await this.login('robot10', '421we2fgv34897');
+
+            // await this.send(groups.nami, 'code', JSON.stringify({
+            //     avatar: 'https://cdn.suisuijiang.com/fiora/avatar/13.jpg',
+            //     username: 'nami - 测试联系人',
+            //     type: 'text',
+            //     content: '测试消息'
+            // }));
         });
         this.socket.on('disconnect', () => {
             this.log('disconnect');
@@ -30,20 +43,28 @@ class Fiora {
             }
         });
     }
+    /**
+     * 日志
+     */
     log(...args) {
         console.log(this.name, ...args);
     }
+    /**
+     * 登录
+     * @param {String} username 用户名
+     * @param {String} password 密码
+     */
     login(username, password) {
         return new Promise((resolve, reject) => {
             this.socket.emit(
                 'login',
                 {
                     username,
-                    password
+                    password,
                 },
                 (res) => {
                     if (typeof res === 'string') {
-                        this.log('error', res);
+                        this.log('登录失败', res);
                         reject(res);
                         return;
                     }
@@ -53,6 +74,12 @@ class Fiora {
             );
         });
     }
+    /**
+     * 发送消息
+     * @param {String} to 群组id
+     * @param {String} type 消息类型 ['text', 'image', 'code', 'url']
+     * @param {String} content 消息内容, image消息需要链接上有width和height, code消息需要再开头用"@[编程语言]@"标明语言
+     */
     send(to, type, content) {
         return new Promise((resolve, reject) => {
             const types = ['text', 'image', 'code', 'url'];
@@ -71,7 +98,7 @@ class Fiora {
                 },
                 res => {
                     if (typeof res === 'string') {
-                        this.log('error', res);
+                        this.log('发送消息失败', res);
                         reject(res);
                         return;
                     }
@@ -81,8 +108,14 @@ class Fiora {
             );
         });
     }
-    listen(linkmanId, cb) {
-        this.listers[linkmanId] = cb;
+    /**
+     * 
+     * @param {String} groupName 群组名
+     * @param {Function} cb 回调
+     */
+    listen(groupName, cb) {
+        const groupId = groups[groupName);]
+        this.listers[groupId] = cb;
         return this;
     }
 }
